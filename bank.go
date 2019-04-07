c609860c8fbb186688c5b07659ceee81e9f162ad
SumedhaJagtap <sumedhajagtap@gmail.com>
2019-04-07 16:03:06 +0530

    changes in writeup

--- bank.go
+++ bank.go
@@ -8,13 +8,11 @@ import (
 
 var mux = sync.Mutex{}
 
-func allocateCust(custNo int, cashierNo int, timePerCustomer int, cashier chan<- int, cashier1 <-chan int) {
+func allocateCust(custNo int, cashierNo int, timePerCustomer int, cashier chan<- int) {
 	mux.Lock()
 	cashier <- custNo
 	fmt.Printf("Cashier %d: Customer %d Started", cashierNo, custNo)
-	no := <-cashier1
-	time.Sleep(timePerCustomer * time.Second)
-	fmt.Printf("Cashier %d: Customer %d Completed", cashierNo, custNo)
+	time.Sleep(3 * time.Second)
 	mux.Unlock()
 }
 
@@ -24,27 +22,36 @@ func main() {
 	c2 := make(chan int)
 	c3 := make(chan int)
 	timePerCustomer := 3
-
+	noCust := 10
 	wg.Add(3)
-	go func(i int) {
-		defer wg.Done()
-		fmt.Printf("Cashier 1: Customer %d Started", i)
-		c1 <- i
-	}(i)
+	for i := 1; i <= noCust; i++ {
+		go func(i int) {
+			defer wg.Done()
+			if _, ok := <-c1; !ok {
 
-	go func(i int) {
-		defer wg.Done()
-		fmt.Printf("Cashier 2: Customer %d Started", i)
-		c3 <- i
-	}(i)
-	go func(i int) {
-		wg.Done()
-		fmt.Printf("Cashier 3: Customer %d Started", i)
-		c2 <- i
-	}(i)
+				allocateCust(i, 1, timePerCustomer, c1)
 
-	wg.Close()
-	for i := 1; i <= 100; i++ {
-		defer wg.Done()
+			} else if _, ok := <-c2; !ok {
+
+				allocateCust(i, 2, timePerCustomer, c1)
+
+			} else if _, ok := <-c3; !ok {
+
+				allocateCust(i, 3, timePerCustomer, c1)
+
+			}
+		}(i)
 	}
+
+	for i := 1; i <= 100; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Printf("Cashier 1: Customer %d Completed", msg1)
+		case msg2 := <-c2:
+			fmt.Printf("Cashier 2: Customer %d Completed", msg2)
+		case msg3 := <-c3:
+			fmt.Printf("Cashier 3: Customer %d Completed", msg3)
+		}
+	}
+	wg.Wait()
 }
